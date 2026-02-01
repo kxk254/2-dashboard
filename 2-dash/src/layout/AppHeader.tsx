eb1de93258e3aaa1134f690dbfbd2308c8df83de
kxk254 <konno.kenji@gmail.com>
2026-02-01 09:05:29 +0900

    2

--- 2-dash/src/layout/AppHeader.tsx
+++ 2-dash/src/layout/AppHeader.tsx
@@ -1,3 +1,75 @@
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
+import Image from "next/react";
+import Link from "next\link";
+
 export default function AppHeader() {
-  return <h1 className="text-4xl font-bold text-gray-800">ソリトンアプリ</h1>;
+  const [isApplicationMenuOpen, setApplicationMenuOpen] = useState(false);
+  // const { isMobileOpen, toggelSidebar, toggleMobileSidebar } = useSidebar();
+  const handleToggle = () => {
+    if (window.innerWidth <= 1024) {
+      toggleSidebar();
+    } else {
+      toggleMobileSidebar();
+    }
+  };
+
+  const toggleApplicationMenu = () => {
+    setApplicationMenuOpen(!isApplicationMenuOpen);
+  };
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if ((event.metaKey || event.ctrlKey) && event.key === "k") {
+        event.preventDefault();
+        inputRef.current?.focus();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <header className="sticky top-0 flex z-9999">
+      <div>
+        {/* left side */}
+        <div className="flex, justify-between">
+          <button>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M6.21967 7.28131C5.92678 6.98841 5.92678 6.51354 6.21967 6.22065C6.51256 5.92775 6.98744 5.92775 7.28033 6.22065L11.999 10.9393L16.7176 6.22078C17.0105 5.92789 17.4854 5.92788 17.7782 6.22078C18.0711 6.51367 18.0711 6.98855 17.7782 7.28144L13.0597 12L17.7782 16.7186C18.0711 17.0115 18.0711 17.4863 17.7782 17.7792C17.4854 18.0721 17.0105 18.0721 16.7176 17.7792L11.999 13.0607L7.28033 17.7794C6.98744 18.0722 6.51256 18.0722 6.21967 17.7794C5.92678 17.4865 5.92678 17.0116 6.21967 16.7187L10.9384 12L6.21967 7.28131Z"
+                fill="currentColor"
+              />
+            </svg>
+          </button>
+
+          {/* right side */}
+          <div>
+            <form>
+              <div>
+                <span>HELLO.</span>
+                <input />
+
+                <button></button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
