8a08bea4269ce4eebf0b355eca4bbe7455c519e9
konno <konno.kenji@gmail.com>
2026-02-08 03:54:57 +0000

    2

--- 2-dash/src/layout/AppHeader.tsx
+++ 2-dash/src/layout/AppHeader.tsx
@@ -55,7 +55,7 @@ const AppHeader: React.FC = () => {
               width={154}
               height={32}
               className="dark:hidden"
-              src="/images/logo/logo.svg"
+              src="/images/logo/logo-soliton.svg"
               alt="logo"
             />
             <Image
