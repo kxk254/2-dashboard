dd6f7cac24712f7009694fcba60675288ecc5320
kxk254 <konno.kenji@gmail.com>
2026-02-11 22:22:48 +0900

    2

--- 2-dash/src/components/ecommerce/StatisticsChart.tsx
+++ 2-dash/src/components/ecommerce/StatisticsChart.tsx
@@ -10,15 +10,106 @@ const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 export default function StatisticsChart() {
   const datePickerRef = useRef<HTMLInputElement>(null);
-  useEffect(() => {}, []);
-  const options: ApexOptions = {};
-  const series = [];
+  useEffect(() => {
+    if (!datePickerRef.current) return;
+
+    const today = new Date();
+    const sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(today.getDate() - 6);
+
+    const fp = flatpickr(datePickerRef.current, {
+      mode: "range",
+      static: true,
+      monthSelectorType: "static",
+      dateFormat: "M d",
+      defaultDate: [sevenDaysAgo, today],
+      clickOpens: true,
+      prevArrow:
+        '<svg class="stroke-current" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M12.5 15L7.5 10L12.5 5" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+      nextArrow:
+        '<svg class="stroke-current" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 15L12.5 10L7.5 5" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+    });
+
+    return () => {
+      if (!Array.isArray(fp)) {
+        fp.destroy();
+      }
+    };
+  }, []);
+  const options: ApexOptions = {
+    legend: {
+      show: false,
+      position: "top",
+      horizontalAlign: "left",
+    },
+    colors: ["#6a67f5"],
+    chart: {
+      fontFamily: "Outfit, sans-serif",
+      height: 310,
+      type: "line",
+      toolbar: { show: false },
+    },
+    stroke: {
+      curve: "straight",
+      width: [2, 2],
+    },
+    fill: {
+      type: "gradient",
+      gradient: { opacityForm: 0.55, opacityTo: 0 },
+    },
+    markers: {
+      size: 0,
+      strokeColors: "fff",
+      strokeWidth: 2,
+      hover: { size: 6 },
+    },
+    grid: {
+      xaxis: { lines: { show: false } },
+      yaxis: { lines: { show: true } },
+    },
+    dataLabels: { enabled: false },
+    tooltip: { enabled: true, x: { format: "dd MMM yyyy" } },
+    xaxis: {
+      type: "category",
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      axisBorder: { show: false },
+      axisTicks: { show: false },
+      tooltip: { enabled: false },
+    },
+    yaxis: {
+      labels: { fontSize: "12px", colors: ["#6b7280"] }, // gray-500
+      title: { test: "", style: { fontSize: "0px" } },
+    },
+  };
+  const series = [
+    {
+      name: "sales",
+      data: [100, 190, 170, 160, 175, 165, 170, 205, 230, 210, 240, 235],
+    },
+    {
+      name: "Revenue",
+      data: [40, 30, 50, 40, 55, 40, 70, 100, 110, 120, 150, 140],
+    },
+  ];
 
   return (
     <div>
       <div>
         <div>
-          <h3></h3>
+          <h3>Statistics</h3>
           <p></p>
         </div>
         <div className="flex items-center gap-3 sm:justify-end">
