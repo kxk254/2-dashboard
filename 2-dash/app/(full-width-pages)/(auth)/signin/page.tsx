7d9155fec11c5560973a08256842fd9c4f40f9b7
kxk254 <konno.kenji@gmail.com>
2026-02-14 11:34:08 +0900

    2

---
+++ 2-dash/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -0,0 +1,10 @@
+import SignInForm from "@/src/components/auth/SignInForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next.js SignIn Page | TailAdmin -Next.js Dashboard Template",
+  description: "This is Next.js Signin Page TailAdmin Dashboard Template",
+};
+export default function Sigin() {
+  return <SignInForm />;
+}
